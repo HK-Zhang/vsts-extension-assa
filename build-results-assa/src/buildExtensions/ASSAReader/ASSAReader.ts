e3549c216f6bec7cd976e52a1753a185d08f757c
HK-Zhang <yxzhk@hotmail.com>
2023-09-11 08:58:06 +0800

    parse yaml

--- build-results-assa/src/buildExtensions/ASSAReader/ASSAReader.ts
+++ build-results-assa/src/buildExtensions/ASSAReader/ASSAReader.ts
@@ -2,12 +2,21 @@ import tl = require("azure-pipelines-task-lib/task");
 import * as fs from 'fs';
 import * as path from 'path';
 
-function run() {
-    const assaPath = tl.getPathInput("assaPath",true);
-    console.log(assaPath);
-    var data = fs.readFileSync(assaPath!,'utf8');
-    console.log(data);
+import yaml = require('js-yaml');
+// import { Assa } from './Assa';
 
+
+function run() {
+    console.log("debug");
+    const assaPath = tl.getPathInput("assaPath",true);
+    // const assaPath = "assa.yml"
+    console.log(assaPath);
+    // var data = fs.readFileSync(assaPath!,'utf8');
+    // var assaData:Assa = YAML.parse(data)
+    var assaData = yaml.load(fs.readFileSync(assaPath!, 'utf8'));
+
+    console.log(assaData);
+    // console.log(data);
     const agentTempDirectory = tl.getVariable('Agent.TempDirectory');
     const jsonReportFullPath = path.join(agentTempDirectory!,`assa.log`);
 
---
+++ build-results-assa/src/buildExtensions/ASSAReader/Assa.ts
@@ -0,0 +1,37 @@
+export interface Assa {
+    assaVersion:        number;
+    projectName:        string;
+    projectVersion:     number;
+    projectDescription: string;
+    owner:              string;
+    status:             string;
+    revisions:          Revision[];
+    azureSubscriptions: any[];
+    endpoints:          string[];
+    repositories:       string[];
+    pipelines:          string[];
+    people:             string[];
+    controls:           Control[];
+}
+
+export interface Control {
+    id:          string;
+    description: string;
+    status:      Status;
+    evidence:    null | string;
+    responsible: string;
+    workItemUrl: null | string;
+}
+
+export enum Status {
+    C = "C",
+    IP = "IP",
+    Na = "NA",
+    Nc = "NC",
+    PC = "PC",
+}
+
+export interface Revision {
+    date:        Date;
+    description: string;
+}
