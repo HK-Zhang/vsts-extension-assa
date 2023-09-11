b151fa5bf8180b063559aa8a9d688775149f38ca
HK-Zhang <yxzhk@hotmail.com>
2023-09-11 13:56:09 +0800

    set control meta data

---
+++ build-results-assa/src/AssaSchema.ts
@@ -0,0 +1,437 @@
+const AssaSchema = {
+  version: "6.0",
+  controls: [
+    {
+      id: "7e768b87-44ca-48e9-9f5e-c67bca160550",
+      ref: "1.1.1",
+      mandatory: true,
+    },
+    {
+      id: "26b2a308-4269-41f8-9da5-78df912d664e",
+      ref: "1.1.2",
+      mandatory: true,
+    },
+    {
+      id: "5fde3e2e-f662-419d-88e6-9ffdd13c1164",
+      ref: "1.1.3",
+      mandatory: true,
+    },
+    {
+      id: "9466c81b-f2f3-4ee9-acca-8cbe2c3f6f59",
+      ref: "1.1.4",
+      mandatory: false,
+    },
+    {
+      id: "2e01495d-69bb-42c9-b07f-e355a1de91f7",
+      ref: "1.1.5",
+      mandatory: false,
+    },
+    {
+      id: "b698fd30-2123-44ca-aee3-f0a27bb60801",
+      ref: "1.2.1",
+      mandatory: true,
+    },
+    {
+      id: "32e570b0-8367-4124-a987-d739d8279198",
+      ref: "1.2.2",
+      mandatory: true,
+    },
+    {
+      id: "b562a0d7-83b0-431c-ab6d-79d902a40e0a",
+      ref: "1.2.3",
+      mandatory: true,
+    },
+    {
+      id: "e22d0454-e964-4f99-b4fb-eff11dfbe053",
+      ref: "1.2.4",
+      mandatory: true,
+    },
+    {
+      id: "7610da80-8695-491d-a1d0-d025880dcdbf",
+      ref: "1.2.5",
+      mandatory: true,
+    },
+    {
+      id: "3d39606f-d146-49da-8f50-203e5f2c15d8",
+      ref: "1.3.1",
+      mandatory: true,
+    },
+    {
+      id: "f7cedad4-8c67-44f7-8002-bda11a3e1c2b",
+      ref: "1.3.2",
+      mandatory: true,
+    },
+    {
+      id: "3f830e0b-7c3d-482f-b6cd-25c82c790885",
+      ref: "1.4.1",
+      mandatory: false,
+    },
+    {
+      id: "dc1bd70a-300b-4510-98e3-c5eec8650fdf",
+      ref: "1.4.2",
+      mandatory: true,
+    },
+    {
+      id: "67c13892-9bd3-406d-b708-e4806762bda5",
+      ref: "1.4.3",
+      mandatory: false,
+    },
+    {
+      id: "7c554430-399f-44fd-b24c-dc9d0aa1ac7a",
+      ref: "1.4.4",
+      mandatory: false,
+    },
+    {
+      id: "da214eb9-aa36-46ce-acb3-5b652e8ac2ab",
+      ref: "1.4.5",
+      mandatory: true,
+    },
+    {
+      id: "9901d6df-dbdd-437f-b42c-804b4f90e76e",
+      ref: "1.5.1",
+      mandatory: false,
+    },
+    {
+      id: "8c3619a7-905d-4ba7-8cc2-4cb8263043c8",
+      ref: "1.5.2",
+      mandatory: true,
+    },
+    {
+      id: "013ed143-67aa-4a49-be81-44e0a99bc677",
+      ref: "1.5.3",
+      mandatory: true,
+    },
+    {
+      id: "7ac8843f-9e8d-4bfe-8562-edb1fb78cd34",
+      ref: "1.5.4",
+      mandatory: true,
+    },
+    {
+      id: "14a54a36-600b-4bf1-8021-f99e2d42d76a",
+      ref: "1.5.5",
+      mandatory: false,
+    },
+    {
+      id: "b3784366-408f-42b4-840b-0f03363b49a6",
+      ref: "1.5.6",
+      mandatory: false,
+    },
+    {
+      id: "8a6cc148-3531-40da-a06f-02226790a5fa",
+      ref: "1.6.1",
+      mandatory: false,
+    },
+    {
+      id: "9d345c08-4d8a-4d48-82f5-bd889d02a439",
+      ref: "2.1.1",
+      mandatory: true,
+    },
+    {
+      id: "e6ffb4af-e354-4a83-b43f-0da106736a55",
+      ref: "2.1.2",
+      mandatory: true,
+    },
+    {
+      id: "ca386ffa-3b95-4cad-a2e6-9b3dbb473339",
+      ref: "2.1.3",
+      mandatory: true,
+    },
+    {
+      id: "bb6e6984-2842-402a-8491-a7a15a663b09",
+      ref: "2.2.1",
+      mandatory: true,
+    },
+    {
+      id: "5253d7ed-ab16-4c11-b5c7-125a50d82855",
+      ref: "2.2.2",
+      mandatory: true,
+    },
+    {
+      id: "dc03a264-62d4-419d-9ff4-038b222748cb",
+      ref: "2.2.3",
+      mandatory: true,
+    },
+    {
+      id: "934690fa-3412-44af-832c-33caf365a3c2",
+      ref: "2.2.4",
+      mandatory: true,
+    },
+    {
+      id: "89f534fc-a320-4342-bc6f-794a46f43382",
+      ref: "2.2.5",
+      mandatory: true,
+    },
+    {
+      id: "d5949ad1-f38f-41c8-a8c3-519644a19ed7",
+      ref: "2.2.6",
+      mandatory: false,
+    },
+    {
+      id: "ee9a47ab-ac6e-45d1-94cd-58c59cacae82",
+      ref: "2.2.7",
+      mandatory: false,
+    },
+    {
+      id: "8284850c-c93a-479f-a919-20761451ca0d",
+      ref: "2.2.8",
+      mandatory: true,
+    },
+    {
+      id: "9168f314-259c-495f-b1d4-def1b88e9fcd",
+      ref: "2.3.1",
+      mandatory: true,
+    },
+    {
+      id: "2bc4ba39-eff0-4a56-8141-41a0503ace75",
+      ref: "2.3.2",
+      mandatory: false,
+    },
+    {
+      id: "74e35ace-d30f-4e23-a85f-3f612d18a940",
+      ref: "2.3.3",
+      mandatory: true,
+    },
+    {
+      id: "2af29dda-e81f-46b4-835c-7126bf685f3a",
+      ref: "2.3.4",
+      mandatory: false,
+    },
+    {
+      id: "c1de556a-dcde-4871-93d8-447d41549158",
+      ref: "2.3.5",
+      mandatory: true,
+    },
+    {
+      id: "aa2218fc-6e4a-47d5-bfb9-2577b3797b02",
+      ref: "2.3.6",
+      mandatory: true,
+    },
+    {
+      id: "1816c4ca-b2eb-40d3-8b7e-dfd885796bf2",
+      ref: "2.3.7",
+      mandatory: true,
+    },
+    {
+      id: "a6b66b3f-ff86-4287-b95f-df263fb8760c",
+      ref: "2.3.8",
+      mandatory: true,
+    },
+    {
+      id: "631fee1e-9bd5-49ed-9d01-413500fd7038",
+      ref: "2.3.9",
+      mandatory: false,
+    },
+    {
+      id: "ad630ed2-dafa-487e-87f3-3d40567dac7d",
+      ref: "2.3.10",
+      mandatory: true,
+    },
+    {
+      id: "22a34fee-1f61-4fa7-88dc-43704de9b657",
+      ref: "2.4.1",
+      mandatory: true,
+    },
+    {
+      id: "11d3b533-be50-480d-8334-24dc5a850b02",
+      ref: "2.4.2",
+      mandatory: true,
+    },
+    {
+      id: "bf145fb1-a38a-4b80-b9ac-fd5225374d2d",
+      ref: "2.4.3",
+      mandatory: true,
+    },
+    {
+      id: "1a2fe961-2dfc-4551-9b08-bd9e884b6f1c",
+      ref: "2.4.4",
+      mandatory: true,
+    },
+    {
+      id: "c5eb86ed-0f64-4133-b0c2-178dfdbf54f1",
+      ref: "2.4.5",
+      mandatory: true,
+    },
+    {
+      id: "5ba75947-a541-4728-8dfa-8fa5634a8b20",
+      ref: "2.4.6",
+      mandatory: true,
+    },
+    {
+      id: "7f8e6762-96a7-4b04-9bde-375039181f2d",
+      ref: "2.4.7",
+      mandatory: true,
+    },
+    {
+      id: "b6b2475c-c1f7-4cf5-add8-bce75e20f1bc",
+      ref: "2.4.8",
+      mandatory: true,
+    },
+    {
+      id: "a9a8cda3-9c47-47f0-95bc-0f5c87d71735",
+      ref: "2.5.1",
+      mandatory: true,
+    },
+    {
+      id: "0033b614-aecc-4bd6-b587-d5eef3895a90",
+      ref: "2.5.2",
+      mandatory: true,
+    },
+    {
+      id: "d7c1d400-9c1c-4fc1-82b1-b45c02782324",
+      ref: "2.5.3",
+      mandatory: true,
+    },
+    {
+      id: "c0156b3b-8421-47c1-b5ae-86c0894a1b65",
+      ref: "2.5.4",
+      mandatory: true,
+    },
+    {
+      id: "d07eff7d-667a-40c2-9570-0b4558ad8e3e",
+      ref: "2.5.5",
+      mandatory: true,
+    },
+    {
+      id: "78783806-f46f-4514-9833-501f237192b8",
+      ref: "2.5.6",
+      mandatory: true,
+    },
+    {
+      id: "b716f710-330e-4245-bc07-79a9dc47907c",
+      ref: "2.6.1",
+      mandatory: true,
+    },
+    {
+      id: "e52b2530-e6b6-45b4-ba66-798f9260a994",
+      ref: "2.6.2",
+      mandatory: true,
+    },
+    {
+      id: "87187450-8640-44ec-944e-7bf1bb287fb5",
+      ref: "2.6.4",
+      mandatory: true,
+    },
+    {
+      id: "c933d26a-2cff-497e-aef0-685b3e24182a",
+      ref: "2.6.5",
+      mandatory: true,
+    },
+    {
+      id: "f779a92e-4f0d-4df9-8d64-2ae2f0c5ee93",
+      ref: "2.6.6",
+      mandatory: true,
+    },
+    {
+      id: "622253be-45f3-4883-ad7d-e8f4cf37f292",
+      ref: "2.6.7",
+      mandatory: true,
+    },
+    {
+      id: "aa31298d-d336-4eb9-ba6c-3bd5e69e0f89",
+      ref: "2.6.8",
+      mandatory: true,
+    },
+    {
+      id: "06725986-85e6-488f-8341-b0e30e7e4d04",
+      ref: "2.6.9",
+      mandatory: true,
+    },
+    {
+      id: "9767dd38-6061-435e-8da1-769b290f38ce",
+      ref: "2.6.10",
+      mandatory: true,
+    },
+    {
+      id: "8e2d0a02-70a8-4aad-a012-aac946d9e00a",
+      ref: "2.6.11",
+      mandatory: true,
+    },
+    {
+      id: "0d923f29-32ff-4c87-9632-785afcdf0dd0",
+      ref: "2.6.12",
+      mandatory: true,
+    },
+    {
+      id: "606b0750-55b8-463f-937a-dbc6c9cd7f05",
+      ref: "2.6.13",
+      mandatory: true,
+    },
+    {
+      id: "ab02059f-dc9f-4084-a42e-a174d3fed45a",
+      ref: "2.6.14",
+      mandatory: true,
+    },
+    {
+      id: "726ee874-f7a5-4a75-9307-ea1093fc7949",
+      ref: "2.7.1",
+      mandatory: true,
+    },
+    {
+      id: "b4d92ce7-e192-4380-b0e6-f21e59185f84",
+      ref: "2.7.2",
+      mandatory: false,
+    },
+    {
+      id: "84b70d3c-125d-4e3a-ba41-d72d53b66abb",
+      ref: "2.7.3",
+      mandatory: true,
+    },
+    {
+      id: "3f444770-b84b-4343-ac3e-1fd3c0c066a3",
+      ref: "2.7.4",
+      mandatory: true,
+    },
+    {
+      id: "eadd8aeb-5b8c-4c9f-ab16-102303e5f5ba",
+      ref: "2.7.5",
+      mandatory: true,
+    },
+    {
+      id: "a621ba2a-80ae-4fcd-af40-28035ba2506c",
+      ref: "2.7.6",
+      mandatory: true,
+    },
+    {
+      id: "a5eb86ed-0f64-4133-b0c2-178dfdbf54f1",
+      ref: "2.7.7",
+      mandatory: true,
+    },
+    {
+      id: "72d9c28e-f95c-4b1d-96a8-81ee1a077c00",
+      ref: "2.7.8",
+      mandatory: true,
+    },
+    {
+      id: "6908ec0b-1ed7-4443-839b-6fab37119878",
+      ref: "2.7.9",
+      mandatory: true,
+    },
+    {
+      id: "6b6bf6d0-bf40-4732-b3ec-a2e61475245c",
+      ref: "3.1.1",
+      mandatory: true,
+    },
+    {
+      id: "7f55e440-7349-4943-b42f-f830573f00a3",
+      ref: "3.1.2",
+      mandatory: true,
+    }
+  ],
+};
+
+export const ControlRelevanceTable = {
+  version: "6.0",
+  controlRelevance: [
+    {
+      threadEvent: "Session hijacking",
+      measurement: [
+        {
+          control: "7e768b87-44ca-48e9-9f5e-c67bca160550",
+          score: 0,
+        },
+      ],
+    },
+  ],
+};
+
+export default AssaSchema;
--- build-results-assa/src/buildExtensions/ASSAReader/ASSAReader.ts
+++ build-results-assa/src/buildExtensions/ASSAReader/ASSAReader.ts
@@ -3,20 +3,18 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 import yaml = require('js-yaml');
-// import { Assa } from './Assa';
+import { Assa } from "./Assa";
 
 
 function run() {
-    console.log("debug");
     const assaPath = tl.getPathInput("assaPath",true);
     // const assaPath = "assa.yml"
     console.log(assaPath);
-    // var data = fs.readFileSync(assaPath!,'utf8');
-    // var assaData:Assa = YAML.parse(data)
-    var assaData = yaml.load(fs.readFileSync(assaPath!, 'utf8'));
+    const assaData = yaml.load(fs.readFileSync(assaPath!, 'utf8')) as Assa;
 
     console.log(assaData);
-    // console.log(data);
+
+    
     const agentTempDirectory = tl.getVariable('Agent.TempDirectory');
     const jsonReportFullPath = path.join(agentTempDirectory!,`assa.log`);
 
